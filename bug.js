307ad2ee5671e1d075077c9aa6f7aa92e076147c
BugHunter-X <askzenai@gmail.com>
2024-12-13 14:00:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,17 @@
+```javascript
+import { useRouter } from 'next/router';
+
+function MyComponent() {
+  const router = useRouter();
+  const handleClick = () => {
+    // This will cause an error if the route doesn't exist
+    router.push('/nonexistent-route'); 
+  };
+
+  return (
+    <button onClick={handleClick}>Go to Nonexistent Route</button>
+  );
+}
+
+export default MyComponent;
+```
